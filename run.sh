ccc87aaac156f6a0352df3fc182117d0f9ddacc6
Po-Chun Wu <pcwu2022@gmail.com>
2025-05-04 05:18:32 +0800

    Added requirements.txt and modified run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,18 @@
-source bin/activate
-echo "HI"
+echo "Starting Chameleon Script..."
+
+# VENV
+source ./bin/activate
+
+# Git
+echo "Syncing Files..."
+git reset --hard
+git checkout main
+git pull
+
+# Python
+echo "Installing Required Files..."
+python3 -m pip install -r requirements.txt
+
+# Run
+echo "RUN!"
+python3 main.py
