13f50864acd39799d91b39152e503f437a9dbab1
jialin <jlfang0119@gmail.com>
2025-05-03 11:51:04 +0800

    added hasHit into protocol

--- rpi/rpi.py
+++ rpi/rpi.py
@@ -4,6 +4,7 @@
 
 def rpi(
     hasCockroach: bool,         # True if cockroach exist
+    hasHit: bool,               # True if has hit cockroach
     cockroachPosition: tuple,   # (xPos: int, yPos: int)
     color: tuple                # (r: int, g: int, b: int)
 ):
